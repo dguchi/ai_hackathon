9034ae8b7e1664f652bb7186d379abe8f739a5ba
dguchi <deugchi.git.hub@gmail.com>
2024-06-23 13:07:26 +0900

    flaskでレンダリング

---
+++ app.py
@@ -0,0 +1,17 @@
+# app.py
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    username = request.form['username']
+    email = request.form['email']
+    return f'ユーザー名: {username}, Eメール: {email}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
